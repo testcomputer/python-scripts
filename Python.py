cf302bacbac23247ca2018ad3c92c8a7273e0d6c
testcomputer div <104815254+testcomputer@users.noreply.github.com>
2022-05-28 06:01:05 -0600

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,13 @@
+print("Welcome to the testcomputer's automated help desk")
+name = input("Please enter your first and last name. ")
+input("Email:")
+input("Phone:")
+Task = input('What is your purpose')
+
+if Task == ("To understand everything")
+print("Understood")
+else: 
+  (input("What is your timeline")
+   
+print(f"Thank you {name} for using Python practice tool")
+   
