e585d3ed5409cf9eceef66d6fe2fe8fa47b0bbf7
DaniJordWileTorr <104815254+testcomputer@users.noreply.github.com>
2022-11-23 19:29:09 -0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -11,3 +11,19 @@ else:
    
 print(f"Thank you {name} for using Python practice tool")
    
+   
+   How to create a user in AD using python
+   
+   
+from pyad import *
+pyad.set_defaults(ldap_server="dc1.domain.com", username="service_account", password="mypassword")
+ou = ADContainer.from_dn("ou=users, dc=domain, dc=com")
+new_user = ADUser.create("Daniel", ou, password="Secret")
+
+
+new_user.set_attribute("mail", "daniel@example.com")
+group = ADGroup.from_dn("so-users")
+group.add_member(new_user)
+
+
+new_user.delete()
