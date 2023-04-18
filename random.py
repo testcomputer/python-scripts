f3ce974d00426239d3e00445aa4237a8a05e8645
admin/root <104815254+testcomputer@users.noreply.github.com>
2023-04-17 23:01:05 -0600

    Create random.py

---
+++ random.py
@@ -0,0 +1,18 @@
+# This is a comment
+# Python code starts below
+
+# Declare a variable and assign it a value
+x = 5
+
+# Print the value of the variable
+print(x)
+
+# Ask for user input
+name = input("What is your name? ")
+
+# Print a message using the user's input
+print("Hello, " + name + "!")
+
+# Use a loop to print numbers from 1 to 10
+for i in range(1, 11):
+    print(i)
